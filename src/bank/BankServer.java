a00c29d65329cc2ec3d01313ea74a554f11a37f6
Aryam <aryamiyezra@gmail.com>
2026-05-10 10:48:03 +0300

    final presentation

--- src/bank/BankServer.java
+++ src/bank/BankServer.java
@@ -9,9 +9,9 @@ public class BankServer {
             BankServiceImpl service = new BankServiceImpl();
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind("BankService", service);
-            System.out.println("=================================================");
-            System.out.println("  Simple Banking RMI Server running on port 1099");
-            System.out.println("=================================================");
+            System.out.println("============================================");
+            System.out.println("  Banking RMI Server running on port 1099   ");
+            System.out.println("============================================");
         } catch (Exception e) {
             System.err.println("Server error: " + e.getMessage());
             e.printStackTrace();
